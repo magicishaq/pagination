85e2380e419d874edd0a6882bf73f8428715779f
magicishaq <ishaqkhan1993@gmail.com>
2021-10-26 00:01:22 +0100

    got card working, need to link image and hyperlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,55 @@
-import React, {useState, useEffect} from 'react' 
-import Pagination from './component/Pagination'
-import Post from './component/Post'
-import './App.scss'
-const url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Pagination from "./component/Pagination";
+import Post from "./component/Post";
+import Card from "./component/Card";
+import "./App.scss";
+const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
 const App = () => {
-const[pokemon, setPokemon] = useState('');
-const[error, setError] = useState(null);
+  const [pokemon, setPokemon] = useState("");
+  const [error, setError] = useState(null);
 
-useEffect(() => {
- fetch(url)
- .then(response => {
-   if(response.ok){
-     return response.json()
-   }else{
-     throw new Error('wrong')
-   }
- })
- .then(response => setPokemon(response.results))
- .catch(error => setError('true'))
-},[])
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("wrong");
+        }
+      })
+      .then((response) => setPokemon(response.results))
+      .catch((error) => setError("true"));
+  }, []);
 
-if(error) {
-  return <div>{error}</div>
-}else{
-  return ( <div>
-    {pokemon.length > 0 ? (
-      <>
-        <Pagination
-          pokemon={pokemon}
-          RenderComponent={Post}
-          title="The First 151 Pokemon"
-          pageLimit={5}
-          dataLimit={10.2}
-        />
-      </>
-    ) : (
-     <h1>No Posts to display</h1>
-    )}
-  </div>)
-}
-}
+  if (error) {
+    return <div>{error}</div>;
+  } else {
+    return (
+      <Router>
+        <Route path="/">
+        <div>
+          {pokemon.length > 0 ? (
+            <>
+              <Pagination
+                pokemon={pokemon}
+                RenderComponent={Post}
+                title="The First 151 Pokemon"
+                pageLimit={5}
+                dataLimit={10.2}
+              />
+            </>
+          ) : (
+            <h1>No Posts to display</h1>
+          )}
+        </div>
+        </Route>
+        <Route path="/card/:id">
+            <Card /> 
+        </Route>
+      </Router>
+    );
+  }
+};
 
-export default App
+export default App;
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
-const Card = ({ pokemonData }) => {
+const Card = () => {
+  const id = useParams();
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const [pokemonData, setPokemonData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    fetch(`${url}${id.id}/`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("something bad happened here");
+        }
+      })
+      .then((response) => setPokemonData(response)).then(response => setLoading(!loading))
+      .catch((error) => setError(error));
+  },[]);
+console.log(pokemonData)
+  if(!loading){
+      return (<div>hello world</div>)
+  }
   return (
     <div id="cards">
-      <figure className={`card card--${pokemonData.types[0].type}`}>
+      <figure className={`card card--${pokemonData.types[0].type.name }`}>
         <div className="card__image-container">
           <img
-            src={pokemonData.sprites.other['offical-artwork']}
+            src={pokemonData.sprites.other["offical-artwork"]}
             alt={pokemonData.name}
             className="card__image"
           />
@@ -14,10 +37,11 @@ const Card = ({ pokemonData }) => {
 
         <figcaption className="card__caption">
           <h1 className="card__name">{pokemonData.name}</h1>
-
+          <h3 className="card__type">
           {pokemonData.types.map((entry) => {
-            return <h3 className="card__type">{entry.type}</h3>;
+          return <span>{entry.type.name} </span> ;
           })}
+          </h3>
           <table className="card__stats">
             <tbody>
               {pokemonData.stats.map((metric) => {
@@ -47,4 +71,4 @@ const Card = ({ pokemonData }) => {
   );
 };
 
-export default Card
+export default Card;
